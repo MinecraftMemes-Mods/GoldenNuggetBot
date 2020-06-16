5d78b4d97ca409d1beb5f58368fb617d53f1b1fb
Xeoth <xeoth@xeoth.me>
2020-06-16 13:28:25 +0200

    fetching works as intended

--- src/database.py
+++ src/database.py
@@ -9,6 +9,11 @@ class Database():
             f'INSERT INTO {table} VALUES("{username}", {amount});')
         self.conn.commit()
 
+    def get(self, table, username) -> int:
+        self.c.execute(
+            f'SELECT nuggets FROM {table} WHERE "username"="{username}"')
+        return self.c.fetchone()[0]
+
     def __init__(self):
         self.conn = sqlite3.connect('../nug.db')
         self.c = self.conn.cursor()
