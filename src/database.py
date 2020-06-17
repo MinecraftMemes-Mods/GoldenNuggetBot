6adb70d47836aea6c18c41cd407471ed6005ea4d
Xeoth <xeoth@xeoth.me>
2020-06-17 21:11:31 +0200

    handling lack of user in database

--- src/database.py
+++ src/database.py
@@ -17,9 +17,10 @@ class Database():
 
     def get(self, table, username) -> int:
         self.c.execute(
-            f'SELECT nuggets FROM {table} WHERE "username"="{username}"')
-        return self.c.fetchone()[0]
+            f'SELECT amount FROM {table} WHERE "username"="{username}"')
+        results = self.c.fetchone()
+        return results[0] if results is not None else None
 
     def __init__(self):
-        self.conn = sqlite3.connect('../nug.db')
+        self.conn = sqlite3.connect('nug.db')
         self.c = self.conn.cursor()
