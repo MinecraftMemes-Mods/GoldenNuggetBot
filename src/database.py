abfee9fe47412b347eb50948bc37ae6043857bb4
Xeoth <xeoth@xeoth.me>
2020-06-18 19:26:28 +0200

    Database done

--- src/database.py
+++ src/database.py
@@ -4,22 +4,45 @@ import sqlite3
 class Database():
     """
     Class used for communicating with the DB
-    Tables: received_nuggets, available_nuggets
-    Columns: username | amount
+    Table: nuggets
+    0|username|TEXT|0||1
+    1|amount_received|INTEGER|0||0
+    2|amount_available|INTEGER|0||0
     """
 
-    def set(self, table, username, amount):
+    def set_available(self, username: str, amount: int) -> None:
+        """Set available nuggets (the ones you can spend)"""
         self.c.execute(f"""
-            INSERT OR REPLACE INTO {table}
-            VALUES("{username}", {amount});
-        """)
+            INSERT INTO nuggets(username, amount_available)
+            VALUES (?, ?)
+            ON CONFLICT(username) DO
+            UPDATE SET amount_available = ?;
+        """, (username, amount, amount))
         self.conn.commit()
 
-    def get(self, table, username) -> int:
+    def set_received(self, username: str, amount: int) -> None:
+        """Set received nuggets"""
+        self.c.execute(f"""
+            INSERT INTO nuggets(username, amount_received)
+            VALUES (?, ?)
+            ON CONFLICT(username) DO
+            UPDATE SET amount_received = ?;
+        """, (username, amount, amount))
+        self.conn.commit()
+
+    def get(self, username: str):
+        """Returns a dictionary with available nuggets and received nuggets or none if user doesn't exist in DB"""
         self.c.execute(
-            f'SELECT amount FROM {table} WHERE "username"="{username}"')
+            f'SELECT amount_received, amount_available FROM nuggets WHERE "username"="{username}"')
         results = self.c.fetchone()
-        return results[0] if results is not None else None
+
+        if results is None:
+            return results
+        else:
+            return {
+                'received': results[0],
+                'available': results[1]
+            }
 
     def __init__(self):
         self.conn = sqlite3.connect('nug.db')
