c9e1b608c99df6a2eabeab7a5b8d6f1a3f8601ca
Xeoth <xeoth@xeoth.me>
2020-06-19 19:06:19 +0200

    missing paren

--- src/bot.py
+++ src/bot.py
@@ -39,22 +39,21 @@ for comment in reddit.subreddit('xeothtest').stream.comments():
     the second argument must either be a number or max, full or all
     i. e. `!nugget max` will transfer all nuggets to poster
     """
-    
 
     try:
         if not re.match(r'!(nug|nugget|gold)( \d)?', comment.body) or not comment.parent().stickied or not comment.parent().author.name == "GoldenNugBot":
             continue
     except AttributeError:  # raised if there is no parent comment
         continue
-        
-    #setting some helpful variables
-    commenter = comment.author.name #person who is giving award
-    op = comment.submission.author.name #person receiving award
-    
+
+    # setting some helpful variables
+    commenter = comment.author.name  # person who is giving award
+    op = comment.submission.author.name  # person receiving award
+
     # exception handling
     # author too young and doesn't meet karma requirements
     # (moved from other exception handling to make it so db entry isn't created if author is invalid)
-    if int((time.time() - comment.author.created_utc) / (60 * 60 * 24)) < 9 and (comment.author.link_karma + comment.author.comment_karma) < 100: #9 days for the first part
+    if int((time.time() - comment.author.created_utc) / (60 * 60 * 24)) < 9 and (comment.author.link_karma + comment.author.comment_karma) < 100:  # 9 days for the first part
         comment.reply("ERROR_MESSAGE")
         continue
 
@@ -62,33 +61,34 @@ for comment in reddit.subreddit('xeothtest').stream.comments():
     if db.get(commenter) == None:
         db.set_available(commenter, 3)
         db.received(commenter, 0)
-    
-    #setting some more helpful variables
+
+    # setting some more helpful variables
     commenter_award_nugs = db.get(commenter)["available"]
-        
+
     # splitting the comment into single words
     # i. e. `!nug 20` will become ['!nug', '20']
-    amount_given = 0 #placeholder, since you can't just declare variables in python REEEEEEEEEEEEEEEEEEEEE
+    # placeholder, since you can't just declare variables in python REEEEEEEEEEEEEEEEEEEEE
+    amount_given = 0
     try:
         amount_given = comment.body.split(' ')[1]
-    except IndexError: #because if someone just did !nug meaning 1 nugget
+    except IndexError:  # because if someone just did !nug meaning 1 nugget
         amount_given = 1
-        
+
     # converts valid text synonyms to their nugget amounts
     if amount_given in ("max", "full", "all", "everything"):
-        amount_given = commenter_award_nugs   
+        amount_given = commenter_award_nugs
 
     # more exception handling
     # invalid gift arg
     if not int_conv(amount_given) or amount_given <= 0:
         comment.reply("ERROR_MESSAGE")
         continue
-        
+
     # checks commenter has enough nuggets to award
     elif commenter_award_nugs < amount_given:
         comment.reply("ERROR MESSAGE")
         continue
-        
+
     # checks poster is not awarding themselves
     elif commenter == op:
         comment.reply("ERROR_MESSAGE")
@@ -98,14 +98,15 @@ for comment in reddit.subreddit('xeothtest').stream.comments():
     if db.get(op) == None:
         db.set_available(op, 3)
         db.set_received(op, 0)
-        
-    #setting some more helpful variables
+
+    # setting some more helpful variables
     op_award_nugs = db.get(op)["available"]
     op_received_nugs = db.get(op)["received"]
-    
-    #reducing commenter's award nugs by the number they give
-    commenter_award_nugs -= amount_given #should moderators have infinite award nugs?
-    
+
+    # reducing commenter's award nugs by the number they give
+    # should moderators have infinite award nugs?
+    commenter_award_nugs -= amount_given
+
     """
     This section gives the OP an award nug for hitting a multiple of 5 received nuggets
     
@@ -123,30 +124,31 @@ for comment in reddit.subreddit('xeothtest').stream.comments():
     This little bit of code accounts for that, by finding the difference needed for the next level, and then seeing how much
     it goes over and adds accordingly. It should work
     """
-    
+
     difFrom5 = 5 - op_received_nugs % 5
     if (amount_given >= difFrom5):
         amount_given -= difFrom5
-        bonusNugs += amount_given // 5 + 1 #hopefully doesn't modify amount_given at all my python is rusty
+        # hopefully doesn't modify amount_given at all my python is rusty
+        bonusNugs += amount_given // 5 + 1
         op_award_nugs += bonusNugs
         amount_given += difFrom5
-    
-    #increasing op's received nugs
+
+    # increasing op's received nugs
     op_received_nugs += amount_given
-    
-    #updating db
+
+    # updating db
     db.set_available(commenter, commenter_award_nugs)
     db.set_received(op, op_received_nugs)
     db.set_received(op, op_award_nugs)
-    
-    #update nugflair
-    
-    #log comment
-    
+
+    # update nugflair
+
+    # log comment
+
     if (bonusNugs == 0):
         comment.reply("SUCCESS_MESSAGE")
     else:
         comment.reply("SUCCESS_MESSAGE")
 
     for post in reddit.subreddit("xeothtest").new(limit=10):
-        comment.mod.distinguish(os.getenv('STICKIED_MESSAGE', sticky=True)
+        comment.mod.distinguish(os.getenv('STICKIED_MESSAGE'), sticky=True)
