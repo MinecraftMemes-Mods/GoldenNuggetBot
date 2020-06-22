1a721b17c4826706ac99428ee0e8c5dfd8531a54
Xeoth <xeoth@xeoth.me>
2020-06-22 14:00:17 +0200

    added error()

--- src/log.py
+++ src/log.py
@@ -6,6 +6,32 @@ class Log():
 
     def info(self, message: str):
         """Display an info-like message"""
-        n = datetime.now()
+        n = datetime.now().isoformat()
         print(
-            f'{n.year}.{n.month}.{n.day} {n.hour}:{n.minute}:{n.second}.{n.microsecond} - {message}')
+            f'{n} - [INFO] - {message}')
+
+    def warn(self, message: str):
+        """Display a warning message"""
+        n = datetime.now().isoformat()
+        print(
+            f'{n} -{self.colors["yellow"]} [WARN]{self.colors["reset"]} - {message}')
+
+    def error(self, message: str):
+        """Display an error message"""
+        n = datetime.now().isoformat()
+        print(
+            f'{n} -{self.colors["red"]} [ERROR]{self.colors["reset"]} - {message}')
+
+    def __init__(self):
+        """Define color codes"""
+        self.colors = {
+            'reset': '\033[m',
+            'green': '\033[32m',
+            'yellow': '\033[33m',
+            'red': '\033[31m',
+            'ok': '\033[34m',
+        }
+
+
+a = Log()
+a.error('yeet')
